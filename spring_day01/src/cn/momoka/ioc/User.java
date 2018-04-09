ed7fbfbbe2c7b7269f43bb0d7575de087539ddd0
momoka <454768497@qq.com>
2018-04-09 18:59:14 +0800

    2018.4.9ioc入门

---
+++ spring_day01/src/cn/momoka/ioc/User.java
@@ -0,0 +1,15 @@
+package cn.momoka.ioc;
+
+public class User {
+	
+	public void add(){
+		System.out.println("add......");
+	}
+	
+	public static void main(String[] args) {
+		//原始做法
+		User user=new User();
+		user.add();
+	}
+
+}
